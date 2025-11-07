4769be9919120057c04dd01abce6b55b8c5146ff
Jason Gaby <jasona.gaby@gmail.com>
2025-11-07 13:58:21 -0500

    Environment settings

--- mysite/storage.py
+++ mysite/storage.py
@@ -5,11 +5,9 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticStorage(S3Boto3Storage):
     bucket_name = settings.AWS_STATIC_STORAGE_BUCKET_NAME
     location = 'static'
-    default_acl = 'public-read'
 
 
 class PublicMediaStorage(S3Boto3Storage):
     bucket_name = settings.AWS_MEDIA_STORAGE_BUCKET_NAME
     location = 'media'
-    default_acl = 'public-read'
     file_overwrite = False
