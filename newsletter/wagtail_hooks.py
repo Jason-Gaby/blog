7aeb9b3c9935955be7b3ec4c0477a0aabb6cb2b8
Jason Gaby <jasona.gaby@gmail.com>
2025-11-25 19:27:16 -0500

    Fixed bug with load /admin when no newsletter page exists.

--- newsletter/wagtail_hooks.py
+++ newsletter/wagtail_hooks.py
@@ -22,7 +22,7 @@ def register_newsletter_page_menu_item():
     #    This prevents the Attribute Error and allows the admin to load so you can create the
     #    parent page first.
     if parent_page is None:
-        return
+        return []
         # By returning nothing (None), no menu item is created, and the hook is safely ignored.
 
     # 3. Construct the URL to the 'Add Child Page' view
