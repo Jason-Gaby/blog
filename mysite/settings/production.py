1e15ab44e5e495cf684059797e8238139f1941db
Jason Gaby <jasona.gaby@gmail.com>
2025-12-31 09:39:41 -0500

    Update production settings.

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -129,5 +129,4 @@ ADMINS=MANAGERS
 # PLOTLY GRAPH SETTINGS
 GRAPH_DIR_NAME = config('GRAPH_DIR_NAME')
 GRAPH_DIR = config('GRAPH_ROOT_DIR')
-if os.path.exists(GRAPH_DIR):
-    sys.path.append(GRAPH_DIR)
+sys.path.append(config('CONTENT_ROOT_DIR'))
