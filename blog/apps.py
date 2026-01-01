2ee11cfbe5703604819811e60684b262d4f431e2
Jason Gaby <jasona.gaby@gmail.com>
2026-01-01 13:10:28 -0500

    Needed full path to env directory

--- blog/apps.py
+++ blog/apps.py
@@ -11,7 +11,7 @@ class BlogConfig(AppConfig):
         import blog_content
 
         # 2. Define where the .env for blog_content is
-        dotenv_path = os.path.join(settings.ENV_DIR, ".env.blog_content")
+        dotenv_path = os.path.join(settings.PROJECT_ROOT, settings.ENV_DIR, ".env.blog_content")
 
         # 3. Inject Wagtail's environment into the package
         # This "re-initializes" the settings object, overriding the
