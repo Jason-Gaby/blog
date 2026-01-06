6e4d1e9fd44e1e43f0b994ce52fd05af4c772d7e
Jason Gaby <jasona.gaby@gmail.com>
2026-01-06 13:34:59 -0500

    Update storage of static files to use manifest...

--- mysite/storage.py
+++ mysite/storage.py
@@ -1,12 +1,20 @@
 from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storage
-from django.contrib.staticfiles.finders import FileSystemFinder
-import os
+from storages.backends.s3boto3 import S3Boto3Storage, S3ManifestStaticStorage
 
 
-class StaticStorage(S3Boto3Storage):
+class StaticStorage(S3ManifestStaticStorage):
     bucket_name = settings.AWS_STATIC_STORAGE_BUCKET_NAME
     location = 'static'
+    default_acl = 'public-read'
+
+    # This overrides the global for static files
+    def get_object_parameters(self, name):
+        params = super().get_object_parameters(name)
+        # Set static files to 1 hour or no-cache instead of 24 hours
+        # This solves ths issue with loading the "Preview" on Wagtail Admin pages during edit.
+        # The body section wouldn't load after 24-hours.
+        params['CacheControl'] = 'max-age=31536000, public, immutable'
+        return params
 
 
 class PublicMediaStorage(S3Boto3Storage):
